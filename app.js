9f1dd53f1b8822cd522d34e09a5ac6dd02f1c197
Linda Marko <lindamarko83@gmail.com>
2022-03-15 08:56:50 +0100

    Add routes modules

--- app.js
+++ app.js
@@ -9,40 +9,10 @@ app.use(cookieParser())
 
 app.set('view engine', 'pug')
 
-// app.use((req, res, next) => {
-//   const err = new Error('Oh no!')
-//   err.status = 500
-//   next(err)
-// })
-
-app.get('/', (req, res) => {
-  const name = req.cookies.username
-  if (name) {
-    res.render('index', { name })
-  } else {
-    res.redirect('/hello')
-  }
-})
-
-app.get('/cards', (req, res) => {
-  res.render('card', { prompt: 'Who is buried in Grants tumb?' })
-})
-app.get('/hello', (req, res) => {
-  const name = req.cookies.username
-  if (name) {
-    res.redirect('/')
-  } else {
-    res.render('hello')
-  }
-})
-app.post('/hello', (req, res) => {
-  res.cookie('username', req.body.username)
-  res.redirect('/')
-})
-app.post('/goodbye', (req, res) => {
-  res.clearCookie('username')
-  res.redirect('/hello')
-})
+const mainRoutes = require('./routes')
+const cardRoutes = require('./routes/cards')
+app.use(mainRoutes)
+app.use('/cards', cardRoutes)
 
 app.use((req, res, next) => {
   const err = new Error('Not Found')
