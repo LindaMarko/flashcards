e0956a00aea313737a78df0404acd3d261bfeae7
Linda Marko <lindamarko83@gmail.com>
2022-03-16 08:46:56 +0100

    Change route parameters and randomize cards

--- routes/cards.js
+++ routes/cards.js
@@ -1,8 +1,39 @@
 const express = require('express')
 const router = express.Router()
+const { data } = require('../data/flashcardData.json')
+// const data = require('../data/flashcardData.json').data
+const { cards } = data
+// constcards= data.cards
 
 router.get('/', (req, res) => {
-  res.render('card', { prompt: 'Who is buried in Grants tumb?' })
+  const numberOfCards = cards.length
+  const flashcardId = Math.floor(Math.random() * numberOfCards)
+  res.redirect(`/cards/${flashcardId}`)
+})
+
+router.get('/:id', (req, res) => {
+  const { side } = req.query
+  console.log(req.query, req.params)
+  const { id } = req.params
+
+  if (!side) {
+    res.redirect(`/cards/${id}?side=question`)
+  }
+  const name = req.cookies.username
+  const text = cards[id][side]
+  const { hint } = cards[id]
+
+  const templateData = { id, text, name }
+  if (side === 'question') {
+    templateData.hint = hint
+    templateData.sideToShow = 'answer'
+    templateData.sideToShowDisplay = 'Answer'
+  } else if (side === 'answer') {
+    templateData.sideToShow = 'question'
+    templateData.sideToShowDisplay = 'Question'
+    console.log(templateData)
+  }
+  res.render('card', templateData)
 })
 
 module.exports = router
